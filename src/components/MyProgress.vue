f8a26e97dd475ba5e86480116e0f3d86012758ca
fishing-null <1134979484@qq.com>
2024-12-24 21:27:43 +0800

    props校验简易写法

---
+++ src/components/MyProgress.vue
@@ -0,0 +1,42 @@
+<script setup>
+  const props = defineProps({
+    width: Number,
+  })
+</script>
+
+<template>
+  <div class="my-progress">
+    <div
+        class="inner"
+        :style="{ width: width + '%' }"
+    >
+      <span> {{props.width}}%</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .my-progress {
+    height: 26px;
+    width: 400px;
+    border-radius: 15px;
+    background-color: #272425;
+    border: 3px solid #272425;
+    box-sizing: border-box;
+    margin-bottom: 30px;
+  }
+  .inner {
+    position: relative;
+    background: #379bff;
+    border-radius: 15px;
+    height: 25px;
+    box-sizing: border-box;
+    left: -3px;
+    top: -2px;
+  }
+  .inner span {
+    position: absolute;
+    right: -30px;
+    top: 26px;
+  }
+</style>
