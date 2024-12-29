6d74214113b645e61396100040b3702c0a148495
zbj <1134979484@qq.com>
2024-12-29 18:40:13 +0800

    pinia 组合式

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,27 @@
 <template>
-  <div class="list">
-<!--    使用子组件-->
-    <MyGoods :imgUrl="goodsList[0].picture"
-             :title="goodsList[0].name"
-             :price="goodsList[0].price" />
-    <MyGoods/>
-    <MyGoods :imgUrl="goodsList[1].picture"
-             :title="goodsList[1].name"
-             :price="goodsList[1].price" />
-    <MyGoods/>
-  </div>
+
+  库存总数:<input type="text" v-model.number="stockStore.stock">
+  <hr/>
+  <Add/>
+  <p>stock的两倍 = {{stockStore.doubleStock}}</p>
+  <hr/>
+  <Sub/>
 
 </template>
 
 <script setup>
-  // 导入组件
-  import MyGoods from "@/components/MyGoods.vue";
-  // 将父组件的值传递给子组件
-
-  const goodsObj = {
-    name: '德国百年工艺高端水晶玻璃红酒杯2支装',
-    price: 139,
-    picture:'https://yanxuan-item.nosdn.127.net/8896b897b3ec6639bbd1134d66b9715c.jpg'
-  }
-  const goodOBj2 = {
-    name: '竹制干泡茶盘正方形沥水茶台品茶盘',
-    price: 135,
-    picture: 'https://yanxuan-item.nosdn.127.net/2d942d6bc94f1e230763e1a5a3b379e1.png'
-  }
-  let goodsList = [goodsObj,goodOBj2]
+  import Add from "@/components/Add.vue";
+  import Sub from "@/components/Sub.vue";
+  import {useStock} from "@/storage/index.js";
+  const stockStore = useStock();
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  .list {
-    width: 1000px;
-    margin: 0 auto;
-    display: flex;
-    flex-wrap: wrap;
+  #app {
+    width: 350px;
+    margin: 100px auto;
+    padding: 5px 10px;
+    border: 1px solid #ccc;
   }
 
 </style>
