3af6ea59d8e6b0da2865793dfe6f890f2f6c51e5
zbj <1134979484@qq.com>
2025-01-01 16:56:58 +0800

    插槽基本使用

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
+  <FancyButton>
+<!--    父组件在这里提供 slot content-->
+    Click me!
+<!--    插槽的内容可以是普通的文本，也可以是html模版内容，甚至可以是组件-->
+    <button>click</button>
+<!--    插槽的内容会在子组件的出口处被渲染出来-->
 
-  库存总数:<input type="text" v-model.number="stockStore.stock">
-  <hr/>
-  <Add/>
-  <p>stock的两倍 = {{stockStore.doubleStock}}</p>
-  <hr/>
-  <Sub/>
+<!--    因为插槽是在父组件中,所以可以访问到父组件的数据作用域-->
+    <p>{{data}}</p>
+  </FancyButton>
 
+
+
+
+<!--  这个组件的插槽中没有任何内容，所以会显示插槽出口处的默认内容-->
+  <FancyButton/>
 </template>
 
 <script setup>
-  import Add from "@/components/Add.vue";
-  import Sub from "@/components/Sub.vue";
-  import {useStock} from "@/storage/index.js";
-  const stockStore = useStock();
+
+import FancyButton from "@/components/FancyButton.vue";
+import {ref} from "vue";
+
+const data = ref('from parent');
 </script>
 
-<style>
-  #app {
-    width: 350px;
-    margin: 100px auto;
-    padding: 5px 10px;
-    border: 1px solid #ccc;
-  }
-
+<style lang="scss" scoped>
 </style>
---
+++ src/components/FancyButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="fancy-button">
+<!--    插槽的出口，父组件的slot content会在这里被渲染-->
+    <slot>
+<!--      插槽出口处的内容是默认内容，会在插槽处没有内容的时候展示-->
+      <p>default content</p>
+    </slot>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
