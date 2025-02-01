68f3760e876d9bc07006c49aa344772452425089
zbj <1134979484@qq.com>
2025-02-01 21:33:03 +0800

    el-table 自定义列

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,69 @@
 <template>
-<!--  shadow属性为插槽带上阴影-->
-  <el-card style="max-width: 480px" shadow="always">
-<!--    header-->
-    <template #header>
-      <div class="card-header">
-        <span>Card name</span>
-      </div>
-    </template>
-<!--    body 默认插槽名为dafault-->
-    <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+<!--
+el-table 表格组件
+      data是一个对象数组，:data绑定数据源
+      stripe属性，为表格带上斑马纹
+-->
 
-<!--    footer -->
-    <template #footer>Footer content</template>
-  </el-card>
+  <el-table :data="tableData" style="width: 100%" stripe>
+<!--    一个el-table-column就是一列-->
+  <!--    prop对应属性名
+          label是列名
+  -->
+    <el-table-column label="日期" width="180">
+      <template #default="{row}">
+        <el-icon>
+          <Timer/>
+        </el-icon>
+        <span>{{row.date}}</span>
+      </template>
+    </el-table-column>
+<!--    自定义列模版-->
+    <el-table-column label="名称" width="180">
+  <!--      通过插槽传入模版-->
+    <!--      通过作用域插槽结构出row（每一行的数据对象）-->
+      <template #default="{row}">
+        <el-tag type="success">{{row.name}}</el-tag>
+      </template>
+    </el-table-column>
+    <el-table-column prop="address" label="地址" />
+    <el-table-column label="操作">
+      <template #default="{row}">
+        <el-icon :size="14">
+          <Delete/>
+        </el-icon>
+        <el-icon>
+          <Edit/>
+        </el-icon>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script setup>
+// 数据源 一般发请求从后端拿到
+import {Delete, Edit, Timer} from "@element-plus/icons-vue";
 
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Sakura',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
 </script>
-
-<style lang="scss" scoped>
-
-</style>
